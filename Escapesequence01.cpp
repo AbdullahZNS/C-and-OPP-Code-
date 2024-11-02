c9cd10affc9f8af0be62acd281889f71f02f2cbe
Abdullah Al Mamun <abdullahmamun.cse20@gmail.com>
2024-11-02 12:58:33 +0600

    Add files via upload

---
+++ Escapesequence01.cpp
@@ -0,0 +1,20 @@
+/*
+Escape sequence -01("\n" is used to create new lone)
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout <<"Name         : Abdullah Al Mamun\n";
+    cout <<"ID           : C201093\n";
+    cout << "Department  : Computer Science and Engineering\n";
+    cout << "institution : International Islamic University Chittagong\n";
+
+
+
+    return 0;
+}
